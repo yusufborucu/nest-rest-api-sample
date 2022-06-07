bd73e5314807eb741036d94682bb82e92d0c00bd
Yusuf Borucu <yusufborucu@gmail.com>
2022-06-07 11:00:41 +0300

    Bug fix and update README.md

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,17 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [TypeOrmModule.forRoot({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "12345678",
+    "database": "nest_rest_api_sample_db",
+    "synchronize": false,
+    "logging": true,
+    "entities": ["dist/**/*.entity.js"]
+  }), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/users/users.dto.ts
+++ src/users/users.dto.ts
@@ -2,5 +2,4 @@ export interface UsersDTO {
   id: number;
   name: string;
   email: string;
-  password: string;
 }
